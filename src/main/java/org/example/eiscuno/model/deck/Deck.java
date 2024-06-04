9447407b856a4f593bd96d95eecff3c38250f94f
Fabian Stiven Valencia Cordoba <fabian.cordoba@correounivalle.edu.co>
2024-06-03 20:56:43 -0500

    Version UNO for students implemented

--- src/main/java/org/example/eiscuno/model/deck/Deck.java
+++ src/main/java/org/example/eiscuno/model/deck/Deck.java
@@ -6,14 +6,23 @@ import org.example.eiscuno.model.card.Card;
 import java.util.Collections;
 import java.util.Stack;
 
+/**
+ * Represents a deck of Uno cards.
+ */
 public class Deck {
     private Stack<Card> deckOfCards;
 
+    /**
+     * Constructs a new deck of Uno cards and initializes it.
+     */
     public Deck() {
         deckOfCards = new Stack<>();
         initializeDeck();
     }
 
+    /**
+     * Initializes the deck with cards based on the EISCUnoEnum values.
+     */
     private void initializeDeck() {
         for (EISCUnoEnum cardEnum : EISCUnoEnum.values()) {
             if (cardEnum.name().startsWith("GREEN_") ||
@@ -25,6 +34,7 @@ public class Deck {
                     cardEnum.name().startsWith("TWO_WILD_DRAW_") ||
                     cardEnum.name().equals("FOUR_WILD_DRAW") ||
                     cardEnum.name().equals("WILD")) {
+
                 Card card = new Card(cardEnum.getFilePath(), cardEnum.name());
                 deckOfCards.push(card);
             }
@@ -32,6 +42,12 @@ public class Deck {
         Collections.shuffle(deckOfCards);
     }
 
+    /**
+     * Takes a card from the top of the deck.
+     *
+     * @return the card from the top of the deck
+     * @throws IllegalStateException if the deck is empty
+     */
     public Card takeCard() {
         if (deckOfCards.isEmpty()) {
             throw new IllegalStateException("No hay más cartas en el mazo.");
@@ -39,6 +55,11 @@ public class Deck {
         return deckOfCards.pop();
     }
 
+    /**
+     * Checks if the deck is empty.
+     *
+     * @return true if the deck is empty, false otherwise
+     */
     public boolean isEmpty() {
         return deckOfCards.isEmpty();
     }
