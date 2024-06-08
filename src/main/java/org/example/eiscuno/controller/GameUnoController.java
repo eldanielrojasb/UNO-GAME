b35f13c6646dadb28196c02456a1914e0de17293
Fabian Stiven Valencia Cordoba <fabian.cordoba@correounivalle.edu.co>
2024-06-08 10:46:28 -0500

    version UNO for students implemented

--- src/main/java/org/example/eiscuno/controller/GameUnoController.java
+++ src/main/java/org/example/eiscuno/controller/GameUnoController.java
@@ -8,6 +8,8 @@ import javafx.scene.layout.GridPane;
 import org.example.eiscuno.model.card.Card;
 import org.example.eiscuno.model.deck.Deck;
 import org.example.eiscuno.model.game.GameUno;
+import org.example.eiscuno.model.machine.ThreadPlayMachine;
+import org.example.eiscuno.model.machine.ThreadSingUNOMachine;
 import org.example.eiscuno.model.player.Player;
 import org.example.eiscuno.model.table.Table;
 
@@ -32,6 +34,9 @@ public class GameUnoController {
     private GameUno gameUno;
     private int posInitCardToShow;
 
+    private ThreadSingUNOMachine threadSingUNOMachine;
+    private ThreadPlayMachine threadPlayMachine;
+
     /**
      * Initializes the controller.
      */
@@ -40,14 +45,21 @@ public class GameUnoController {
         initVariables();
         this.gameUno.startGame();
         printCardsHumanPlayer();
+
+        threadSingUNOMachine = new ThreadSingUNOMachine(this.humanPlayer.getCardsPlayer());
+        Thread t = new Thread(threadSingUNOMachine, "ThreadSingUNO");
+        t.start();
+
+        threadPlayMachine = new ThreadPlayMachine(this.table, this.machinePlayer, this.tableImageView);
+        threadPlayMachine.start();
     }
 
     /**
      * Initializes the variables for the game.
      */
     private void initVariables() {
-        this.humanPlayer = new Player();
-        this.machinePlayer = new Player();
+        this.humanPlayer = new Player("HUMAN_PLAYER");
+        this.machinePlayer = new Player("MACHINE_PLAYER");
         this.deck = new Deck();
         this.table = new Table();
         this.gameUno = new GameUno(this.humanPlayer, this.machinePlayer, this.deck, this.table);
@@ -70,6 +82,7 @@ public class GameUnoController {
                 gameUno.playCard(card);
                 tableImageView.setImage(card.getImage());
                 humanPlayer.removeCard(findPosCardsHumanPlayer(card));
+                threadPlayMachine.setHasPlayerPlayed(true);
                 printCardsHumanPlayer();
             });
 
